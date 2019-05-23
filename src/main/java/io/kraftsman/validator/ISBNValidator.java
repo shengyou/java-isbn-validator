5276d474cbf55a246e33520a076d3682cfbd6473
Shengyou Fan <shengyoufan@gmail.com>
2019-05-23 15:52:48 +0800

    refactoring: should pass when enter a validate 13 digit ISBN

--- src/main/java/io/kraftsman/validator/ISBNValidator.java
+++ src/main/java/io/kraftsman/validator/ISBNValidator.java
@@ -2,6 +2,14 @@ package io.kraftsman.validator;
 
 public class ISBNValidator {
     public boolean validate(String isbn) {
-        return true;
+        int total = 0;
+        for (int i = 0; i < 13; i++) {
+            int a = Character.getNumericValue(isbn.charAt(i));
+            int b = (i % 2 == 0) ? 1 : 3;
+
+            total += a * b;
+        }
+
+        return (total % 10 == 0);
     }
 }
